640b1143b8741d2b4d337c673c0f4ac11a01ae93
Lachlan Richards <lachlanric@outlook.com>
2025-11-10 21:59:36 +1100

    Added interactive serial connection

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const { SerialPort } = require("serialport");
-// const { DelimiterParser  } = require("@serialport/parser-regex");
 const { DelimiterParser } = require("@serialport/parser-delimiter");
 
 const app = express();
@@ -9,68 +8,46 @@ const expressWs = require("express-ws")(app);
 app.use(express.json());
 app.use(express.static("public")); // 'public' is the name of your static directory
 
-// const serialport = new SerialPort({ path: "COM6", baudRate: 2000000 })
-// const serialport = new SerialPort({ path: "COM6", baudRate: 1843200 });
-// const serialport = new SerialPort({ path: "COM6", baudRate: 1500000 });
-const serialport = new SerialPort({ path: "COM6", baudRate: 921600 });
-// const serialport = new SerialPort({ path: "COM6", baudRate: 115200 });
+// const SERIAL_PORT = new SerialPort({ path: "COM6", baudRate: 2000000 });
+// const SERIAL_PORT = new SerialPort({ path: "COM6", baudRate: 1843200 });
+// const SERIAL_PORT = new SerialPort({ path: "COM6", baudRate: 1500000 });
+// const SERIAL_PORT = new SerialPort({ path: "COM6", baudRate: 115200 });
 
-// const parser = serialport.pipe(new DelimiterParser({ delimiter: Buffer.from(">>>") })); // ">>>" in hex
-// parser.on("data", (preceeding) => {
-//   const beginStr = "3c3c3c"; // "<<<" in hex
-//   const bufferStr = preceeding.toString("hex");
-//   if (bufferStr.includes(beginStr)) {
-//     const hexDataStr = bufferStr.split(beginStr).slice(1).join("");
-//     console.log("");
-//     console.log("Msg type: " + getMsgType(hexDataStr));
-//     console.log("Floats: " + getFloatData(hexDataStr));
-//   }
-// });
+let SERIAL_PORT = null;
+let WS_HANDLE = null;
 
-let ws_handle = null;
-
-const parser = serialport.pipe(new DelimiterParser({ delimiter: Buffer.from(">>>") })); // ">>>" in hex
-parser.on("data", (preceeding) => {
-  const beginStr = "<<<";
-  const bufferStr = preceeding.toString("ascii");
-  if (!bufferStr.includes(beginStr)) {
-    return;
-  }
-  const jsonStr = bufferStr.split(beginStr).slice(1).join("");
-
-  // Forward onto the browser
-  try {
-    if (ws_handle && JSON.parse(jsonStr)) {
-      ws_handle.send(jsonStr);
+function init_parsers() {
+  const parser = SERIAL_PORT.pipe(new DelimiterParser({ delimiter: Buffer.from(">>>") })); // ">>>" in hex
+  parser.on("data", (preceeding) => {
+    const beginStr = "<<<";
+    const bufferStr = preceeding.toString("ascii");
+    if (!bufferStr.includes(beginStr)) {
+      return;
     }
-  } catch {
-    console.log("Dropped bad msg.");
-  }
-});
+    const jsonStr = bufferStr.split(beginStr).slice(1).join("");
 
-const cameraParser = serialport.pipe(new DelimiterParser({ delimiter: Buffer.from("$$$$$") })); // ">>>" in hex
-cameraParser.on("data", (preceeding) => {
-  const beginPattern = Buffer.from("#####");
-  // console.log("Preceeding");
-  // console.log(preceeding);
-  const startIdx = preceeding.indexOf(beginPattern);
-  // console.log("Index of start: " + startIdx);
-  const jpegData = preceeding.slice(startIdx + 5);
+    // Forward onto the browser
+    try {
+      if (WS_HANDLE && JSON.parse(jsonStr)) {
+        WS_HANDLE.send(jsonStr);
+      }
+    } catch {
+      console.log("Dropped bad msg.");
+    }
+  });
 
-  // Forward onto the browser
-  if (ws_handle) {
-    ws_handle.send(jpegData);
-  }
+  const cameraParser = SERIAL_PORT.pipe(new DelimiterParser({ delimiter: Buffer.from("$$$$$") })); // ">>>" in hex
+  cameraParser.on("data", (preceeding) => {
+    const beginPattern = Buffer.from("#####");
+    const startIdx = preceeding.indexOf(beginPattern);
+    const jpegData = preceeding.slice(startIdx + 5);
 
-  // console.log(jpegData);
-  // console.log("Received camera frame");
-});
-
-// // Read data that is available but keep the stream in "paused mode"
-// serialport.on("readable", function () {
-//   process.stdout.write(serialport.read());
-// });
-
+    // Forward onto the browser
+    if (WS_HANDLE) {
+      WS_HANDLE.send(jpegData);
+    }
+  });
+}
 class MSG_TYPES {
   static SERIAL_DRONE_PARAMS = 0;
   static SERIAL_DRONE_DATA = 1;
@@ -79,38 +56,6 @@ class MSG_TYPES {
   static SERIAL_JOYSTICK = 4;
   static SERIAL_REQUEST_DRONE_PARAMS = 5;
   static SERIAL_RECEIVE_DRONE_PARAMS = 6;
-}
-
-function getMsgType(hexDataStr) {
-  return Uint8Array.from(Buffer.from(hexDataStr.slice(0, 2), "hex"))[0];
-}
-
-function getFloatData(hexDataStr) {
-  return hexStringToFloat32Array(hexDataStr.slice(2));
-}
-
-function hexStringToFloat32Array(hexString) {
-  // Ensure the hex string has an even length
-  if (hexString.length % 2 !== 0) {
-    throw new Error("Hex string must have an even length.");
-  }
-
-  const byteLength = hexString.length / 2;
-  const buffer = new ArrayBuffer(byteLength);
-  const uint8View = new Uint8Array(buffer);
-
-  // Populate the Uint8Array with bytes from the hex string
-  for (let i = 0; i < byteLength; i++) {
-    const byteHex = hexString.substring(i * 2, i * 2 + 2);
-    uint8View[i] = parseInt(byteHex, 16);
-  }
-
-  // Create a Float32Array view over the same buffer
-  // The endianness of the Float32Array will depend on the system's native endianness
-  // or how the original hex string was generated.
-  const float32Array = new Float32Array(buffer);
-
-  return Array.from(float32Array);
 }
 
 function msg_type_byte(msg_type) {
@@ -122,27 +67,73 @@ function floats_to_buf(data) {
   return Buffer.from(new Float32Array(data).buffer);
 }
 
+function close_serial(callback) {
+  if (SERIAL_PORT != null) {
+    SERIAL_PORT.close(() => {
+      console.log("Serial port closed");
+      WS_HANDLE.send(JSON.stringify({ msg_type: MSG_TYPES.SERIAL_COMMS_MSG, data: "Serial port closed" }));
+      SERIAL_PORT = null;
+      if (callback) {
+        callback();
+      }
+    });
+  } else {
+    if (callback) {
+      callback();
+    }
+  }
+}
+
 app.post("/params", (req, res) => {
   const { data } = req.body;
-  serialport.write(Buffer.concat([msg_type_byte(MSG_TYPES.SERIAL_DRONE_PARAMS), floats_to_buf(data)]));
+  SERIAL_PORT.write(Buffer.concat([msg_type_byte(MSG_TYPES.SERIAL_DRONE_PARAMS), floats_to_buf(data)]));
   res.status(200).json({ message: "Received" });
 });
 
 app.post("/calibrate", (req, res) => {
-  serialport.write(msg_type_byte(MSG_TYPES.SERIAL_DRONE_CALIBRATE));
+  SERIAL_PORT.write(msg_type_byte(MSG_TYPES.SERIAL_DRONE_CALIBRATE));
   res.status(200).json({ message: "Calibrated" });
 });
 
 app.post("/obtain_pid", (req, res) => {
-  serialport.write(msg_type_byte(MSG_TYPES.SERIAL_REQUEST_DRONE_PARAMS));
+  SERIAL_PORT.write(msg_type_byte(MSG_TYPES.SERIAL_REQUEST_DRONE_PARAMS));
   res.status(200).json({ message: "Requested PID params" });
+});
+
+app.post("/serial_close", (req, res) => {
+  close_serial();
+  res.status(200).json({ message: "Received" });
+});
+
+app.post("/serial_open", (req, res) => {
+  const { port, baudRate } = req.body;
+  close_serial(() => {
+    SERIAL_PORT = new SerialPort({ path: port, baudRate: baudRate }, (err) => {
+      let msg;
+      if (err) {
+        msg = `${err}`;
+      } else {
+        msg = `Serial port ${port} opened with baud rate ${baudRate}`;
+      }
+      console.log(msg);
+      WS_HANDLE.send(JSON.stringify({ msg_type: MSG_TYPES.SERIAL_COMMS_MSG, data: msg }));
+    });
+    init_parsers();
+    res.status(200).json({ message: "Received" });
+  });
+});
+
+app.get("/serial_list", async (req, res) => {
+  const available_ports = await SerialPort.list();
+  const res_lst = available_ports.map((p) => p.path);
+  res.send(JSON.stringify({ ports: res_lst }));
 });
 
 app.ws("/comms", (ws, req) => {
   ws.on("message", (msg) => {
     console.log(msg);
   });
-  ws_handle = ws;
+  WS_HANDLE = ws;
   console.log("Connected to ws");
 });
 
@@ -151,3 +142,8 @@ app.get("/", (req, res) => {
 });
 
 app.listen(3000);
+
+(async function () {
+  console.log("SerialPort.list()");
+  console.log(await SerialPort.list());
+})();
